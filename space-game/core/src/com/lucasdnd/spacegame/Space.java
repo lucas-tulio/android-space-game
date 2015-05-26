56e6c77e081912e4ef5e9f16b069232577f555b5
Tulio <tulio@doubleleft.com>
2015-05-26 13:48:45 -0300

    Improved Space class

--- space-game/core/src/com/lucasdnd/spacegame/Space.java
+++ space-game/core/src/com/lucasdnd/spacegame/Space.java
@@ -1,12 +1,28 @@
 package com.lucasdnd.spacegame;
 
+import java.util.Random;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Space extends Entity {
 	
-	public Space(float x, float y) {
-		this.x = x;
-		this.y = y;
+	int numStars;
+	float x[], y[];
+	
+	public Space(int numStars) {
+		
+		this.numStars = numStars;
+		x = new float[numStars];
+		y = new float[numStars];
+		
+		Random r = new Random();
+		for (int i = 0; i < numStars; i++) {
+			x[i] = r.nextInt(Gdx.graphics.getWidth());
+			y[i] = r.nextInt(Gdx.graphics.getHeight());
+		}
 	}
 	
 	@Override
@@ -17,7 +33,12 @@ public class Space extends Entity {
 	
 	@Override
 	public void render(ShapeRenderer shapeRenderer) {
-		shapeRenderer.point(x, y, 0);
+		shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.begin(ShapeType.Point);
+        for (int i = 0; i < numStars; i++) {
+        	shapeRenderer.point(x[i], y[i], 0);
+		}
+		shapeRenderer.end();
 	}
 
 }
